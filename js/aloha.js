0d149501cb27736657013a68e7940225a05a9080
Jasmine Barzegar <jasminerbarzy@gmail.com>
2019-01-19 13:53:15 -0800

    rework header

--- js/aloha.js
+++ js/aloha.js
@@ -1,3 +1,4 @@
+$(function() {
 $('.product-section').flickity({
     draggable: true,
     freeScroll: true,
@@ -5,3 +6,25 @@ $('.product-section').flickity({
     cellAlign: 'left',
     contain: true,
   });
+
+
+  /* 
+  * for smooth scrolling 
+  * @source: https://css-tricks.com/smooth-scrolling-accessibility/
+  */
+
+
+    $('a[href*="#"]:not([href="#"])').click(function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          $('html, body').animate({
+            scrollTop: target.offset().top
+          }, 1000);
+          return false;
+        }
+      }
+    });
+  });
+
