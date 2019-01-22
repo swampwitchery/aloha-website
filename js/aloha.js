73c7ad6f3ebd5919d3af2c1e5cd3f86215228eeb
Jasmine Barzegar <jasminerbarzy@gmail.com>
2019-01-22 08:47:09 -0800

    jquery email fix

--- js/aloha.js
+++ js/aloha.js
@@ -1,5 +1,5 @@
-$(function() {
-$('.product-section').flickity({
+$(function () {
+  $('.product-section').flickity({
     draggable: true,
     freeScroll: true,
     prevNextButtons: false,
@@ -7,50 +7,51 @@ $('.product-section').flickity({
     contain: true,
   });
 
-  
+
 
   /* 
-  * for smooth scrolling 
-  * @source: https://css-tricks.com/smooth-scrolling-accessibility/
-  */
+   * for smooth scrolling 
+   * @source: https://css-tricks.com/smooth-scrolling-accessibility/
+   */
 
 
-    $('a[href*="#"]:not([href="#"])').click(function() {
-      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-        var target = $(this.hash);
-        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-        if (target.length) {
-          $('html, body').animate({
-            scrollTop: target.offset().top
-          }, 1000);
-          return false;
-        }
-      }    const sEmail = $(".email").val();
+  $('a[href*="#"]:not([href="#"])').click(function () {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+    const sEmail = $(".email").val();
 
-    });
   });
+});
 
 //email validation
 //http://www.jquerybyexample.net/2011/04/validate-email-address-using-jquery.html
 
-$(".submit").on("click", function() {
-  function validateEmail(sEmail) {
-    var filter = /^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
-    console.log(filter.test(sEmail));
-    if (filter.test(sEmail) && $.trim(sEmail).length !== 0)
-    {
-      return true;
-    } else {
-      return false;
-    }
-  }
-  const sEmail = $(".email").val();
- 
+$(".submit").on("click", ".button", function (event) {
+  event.preventDefault()
+  const sEmail = $(".email-sub").val();
+  event.preventDefault()
+
   if (validateEmail(sEmail)) {
     alert("Thanks for Subscribing!");
   } else {
     alert("Invalid Email");
   }
- });
+});
 
- 
+function validateEmail(sEmail) {
+  var filter = /^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
+  console.log(filter.test(sEmail));
+  if (filter.test(sEmail) && $.trim(sEmail).length !== 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
